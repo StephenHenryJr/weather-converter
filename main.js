99bb95412dcc724dcb224ad61cf3322879ae734d
StephenHenryJr <shenryjr_85@hotmail.com>
2022-06-08 18:34:47 -0400

    js added to add inner html to body

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 //Base Kelvin temperature
-const kelvin = 300;
-// Kelvin converted to Celsius
-let celsius = kelvin - 273;
-// Celsius converted to Farenheit
-let fahrenheit = celsius * (9/5) + 32;
-//Rounded Fahrenheit  temperature
-fahrenheit = Math.floor(fahrenheit);
+// let kelvin = document.getElementById('input').value;
+let kelvin = 0;
 
-console.log(`The temperature is ${celsius} degrees celsius`);
-console.log(`In Fahrenheit, that would be ${fahrenheit} degrees`);
+let convert = () => {
+  if (typeof kelvin !== 'number') {
+    console.log('Enter a valid number')
+  }
+  // Kelvin converted to Celsius
+  let celsius = kelvin - 273;
+  console.log(celsius)
+  // Celsius converted to Farenheit
+  let fahrenheit = celsius * (9 / 5) + 32;
+  console.log(fahrenheit)
+  //Rounded Fahrenheit  temperature
+  fahrenheit = Math.floor(fahrenheit);
+  // Bonus: Celsius converted to Newton
+  let newton = celsius * (33 / 100);
+  newton = Math.floor(newton);
+  console.log(newton)
 
-// Bonus: Celsius converted to Newton
-let newton = celsius * (33/100);
-newton = Math.floor(newton);
-console.log(`For shits and giggles, that would also be ${newton} Newton degrees`);
+  document.getElementById('celsius').innerHTML = `${celsius}`;
+  document.getElementById('fahrenheit').innerHTML = `${fahrenheit}`;
+  document.getElementById('newton').innerHTML = `${newton}`;
+}
+convert();
+// Grab whatever is input - done
+// make sure it's a number
+// convert them to all 3 units 
+// place them in the html  
+// fire off function on click
